31419ff61dcb46713622416197aaecfb06602283
Damiano <damybert@gmail.com>
2022-11-30 11:13:17 +0100

    first commit on github joystick and power buttons working

---
+++ Debug/command_matrices.d
@@ -0,0 +1,9 @@
+# FIXED
+
+command_matrices.obj: ../command_matrices.c
+command_matrices.obj: ../command_matrices.h
+
+../command_matrices.c:
+
+../command_matrices.h:
+
---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -llibc.a -l"/Applications/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/lib/ccs/m4f/grlib.a" -l"/Applications/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib"
+
