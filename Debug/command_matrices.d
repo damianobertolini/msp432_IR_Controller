4201a42f0719fe37ffae9d2b357697059dad9f5b
Damiano <damybert@gmail.com>
2022-11-30 11:44:01 +0100

    caio

--- Debug/command_matrices.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-command_matrices.obj: ../command_matrices.c
-command_matrices.obj: ../command_matrices.h
-
-../command_matrices.c:
-
-../command_matrices.h:
-
--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"/Applications/simplelink_msp432p4_sdk_3_40_01_02/source/ti/grlib/lib/ccs/m4f/grlib.a" -l"/Applications/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib"
-
